505d454ee97e3578295839b41de5c29d6b1e5ea6
ChallapalliSujitha <challapallisujitha567@gmail.com>
2024-08-05 11:37:04 +0530

    suji5

---
+++ app.js
@@ -0,0 +1,10 @@
+// app.js
+const mathOperation = require('./mathsfifth');
+
+const lov1 = 86;
+const lov2 = 44;
+
+console.log(`Addition: ${mathOperation.add(lov1, lov2)}`);
+console.log(`Subtraction: ${mathOperation.subtract(lov1, lov2)}`);
+console.log(`Multiplication: ${mathOperation.multiply(lov1, lov2)}`);
+console.log(`Division: ${mathOperation.divide(lov1, lov2)}`);
